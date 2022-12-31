ec8c8b1cc0f75cb4d6e850e36ef8dc0060a747d5
Nata19754 <chavesnatally0419@gmail.com>
2022-12-31 10:42:17 -0800

    added images-completed project

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useState } from "react"
 import "./App.css"
 import LightSwitch from "./components/LightSwitch"
 
+
 const App = () => {
-  const [lightSwitchStatus, setLightSwitchStatus] = useState("off")
+  // const [lightSwitchStatus, setLightSwitchStatus] = useState("off")
 
   return (
     <>
       <h1>Lightbulb Challenge</h1>
-      <h2>Click on the boxes to turn the light on and off!</h2>
-      <LightSwitch />
+      <h2>Click on the switch to turn the lightbulb on and off !!</h2>
       <LightSwitch />
       <LightSwitch />
       <LightSwitch />
--- src/components/LightSwitch.js
+++ src/components/LightSwitch.js
@@ -1,27 +1,32 @@
 import React, { useState } from "react"
-
+import lightOff from "../assets/lightOff.png"
+import lightOn from "../assets/lightOn.png"
+import switchOff from "../assets/switchOff.png"
+import switchOn from "../assets/switchOn.png"
 
 const LightSwitch = () => {
 
 const [lightSwitchStatus, setLightSwitchStatus]= useState("off")
-const [lighColor, setLightColor] =useState("white")
+// const [lighColor, setLightColor] =useState("white")
 
 const onOff = () => {
-  if (lightSwitchStatus==="off"){
-    setLightColor("yellow")
-    setLightSwitchStatus("on")
-  } else {
-    setLightColor("white")
-    setLightSwitchStatus("off")
-  }
+ setLightSwitchStatus(lightSwitchStatus === "off"? "on" : "off");
+ 
 }
 
   return (
     <>
-      <div className="lightbulb"
+      {/* <div className="lightbulb"
       onClick={onOff}
-      style={{backgroundColor:lighColor}}>
+      style={{backgroundColor:lightColor}}>
         {lightSwitchStatus}
+      </div> */}
+
+      <div>
+
+        <img src={lightSwitchStatus === "off" ? switchOff : switchOn} onClick={onOff} />
+        <img src={lightSwitchStatus == "off" ? lightOff : lightOn} />
+        
       </div>
     </>
   )
